80c694e897fc6301988572f8913350f6743ee470
SBKubric <sbkubric@gmail.com>
2016-10-03 18:54:00 +0300

    Solved the issue

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,20 @@ import json
 
 
 def load_data(filepath):
-    pass
-
+    if filepath != '':
+        with open(filepath) as data_file:
+            return json.load(data_file)
+    else:
+        return -1
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    pass
+    data = -1
+    while data == -1:
+        print('Enter path to the file:')
+        data = load_data(input())
+
+    pretty_print_json(data)
