56479d5f5630130a44745d7044be5bd38975102d
SBKubric <sbkubric@gmail.com>
2016-10-24 23:19:58 +0300

    Fixed README and None absence

--- pprint_json.py
+++ pprint_json.py
@@ -1,21 +1,23 @@
 import json
+import os
 
 
 def load_data(filepath):
-    if filepath != '':
-        with open(filepath) as data_file:
+    if not os.path.isfile(filepath):
+        return None
+    with open(filepath) as data_file:
             return json.load(data_file)
-    else:
-        return -1
+
 
 def pretty_print_json(data):
     print(json.dumps(data, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    data = -1
-    while data == -1:
-        print('Enter path to the file:')
+    print('Enter path to the file:', end='')
+    data = load_data(input())
+    while data is None:
+        print('Looks like the filepath is wrong. Please, enter the real path to JSON file.')
         data = load_data(input())
 
     pretty_print_json(data)
